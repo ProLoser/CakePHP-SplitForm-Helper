900b4da3eb04ecf931de3ab0450d49101632d62d
Dean Sofer <proloser@hotmail.com>
2010-11-09 19:01:17 -0800

    Created a case for arrays passed. Not being used currently

--- views/helpers/split_form.php
+++ views/helpers/split_form.php
@@ -27,26 +27,32 @@ class SplitFormHelper extends AppHelper {
 			$result .= $this->Form->hidden($fieldName . '.' . $fieldName);
 		}
 		
-		if (!isset($options['label'])) {
-			$view =& ClassRegistry::getObject('view');
-			$varName = Inflector::variable(
-				Inflector::pluralize(preg_replace('/_id$/', '', $fieldName))
-			);
-			$varOptions = $view->getVar($varName);
-			$options['label'] = $varOptions[$values];
+		if (is_array($values)) {
+			
+		} else {
+			if (!isset($options['label'])) {
+				$view =& ClassRegistry::getObject('view');
+				$varName = Inflector::variable(
+					Inflector::pluralize(preg_replace('/_id$/', '', $fieldName))
+				);
+				$varOptions = $view->getVar($varName);
+				$options['label'] = $varOptions[$values];
+			}
+			
+			$options['hiddenField'] = false;
+			$options['value'] = $values;
+			$options['name'] = "data[{$fieldName}][{$fieldName}][]";
+			$options['type'] = 'checkbox';
+			if (!isset($options['checked'])) {
+				$options['checked'] = (
+					isset($this->data[$fieldName][$fieldName])
+					&& is_array($this->data[$fieldName][$fieldName])
+					&& in_array($values, $this->data[$fieldName][$fieldName])
+				) ? true : false;
+			}
+			$result .= $this->Form->input($fieldName . $values, $options);
 		}
 		
-		$options['hiddenField'] = false;
-		$options['value'] = $values;
-		$options['name'] = "data[{$fieldName}][{$fieldName}][]";
-		$options['type'] = 'checkbox';
-		$options['checked'] = (
-			isset($this->data[$fieldName][$fieldName])
-			&& is_array($this->data[$fieldName][$fieldName])
-			&& !in_array($values, $this->data[$fieldName][$fieldName])
-		) ? false : true;
-		
-		$result .= $this->Form->input($fieldName . $values, $options);
 		return $result;
 	}
 	
